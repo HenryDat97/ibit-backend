f65d2ecd80ada78d8ca800c191f83b918be7f1f3
hello123-vn <giadat547@gmail.com>
2022-09-25 17:17:10 +0700

    Log

--- controller/posts.js
+++ controller/posts.js
@@ -69,6 +69,7 @@ export const createUser = async (req, res) => {
         const newUser = req.body
         const user = await new UserModel(newUser)
 
+        console.log(req.body)
         res.status(200).json(user)
     } catch (err) {
         console.log(err)
